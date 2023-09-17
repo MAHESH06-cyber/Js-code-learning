cdd86baa99851e2faedbf9f4e195102a091e772c
MAHESH06-cyber <mchaudhari1201@gmail.com>
2023-09-17 22:01:14 +0530

    conversion and ++,--

--- 02_dataTypes/02_Conversion_Operation.js
+++ 02_dataTypes/02_Conversion_Operation.js
@@ -1,10 +1,10 @@
 let score = "40"
 
-console.log(typeof (score));
-console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "44abs" => NaN
@@ -15,7 +15,13 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true & 0 =>false
 // "" => false & "dhfsd" => true
+
+// *******************Operations*********************
+
+let value = 3
+let negValue = -value
+console.log(negValue); 
