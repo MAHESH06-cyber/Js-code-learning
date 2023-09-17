60f23ef1548ba1e71c1daa46627086b910385422
MAHESH06-cyber <mchaudhari1201@gmail.com>
2023-09-17 20:40:44 +0530

    let and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 324562
+let accountEmail = "mahesh12@gmail.com"
+var accountPassword = "qwe@12"
+accountCity = "Nashik"
+
+
+// accountId = 2 // not allowed 
+
+accountEmail = "nte@tm.com"
+accountPassword = "ret@12"
+accountCity = "raipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// prefer not to use var beacuse  its scope issue
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("hello there !")
+const age = 12
