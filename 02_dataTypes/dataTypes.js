12e84d9087c42ab52c51fdba08ae6f4c5a00d65a
MAHESH06-cyber <mchaudhari1201@gmail.com>
2023-09-17 21:08:42 +0530

    datatypes explore

---
+++ 02_dataTypes/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code in new version
+
+//  code readability takes priority
+
+let name = "kunal"
+let age = 21
+let isSingle = true
+let city;
+
+// number
+// bigint
+// string
+// boolean ==> true/false
+// null    ==> standalone value
+// undefiend => not defiend value
+// symbols => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefiend
+
