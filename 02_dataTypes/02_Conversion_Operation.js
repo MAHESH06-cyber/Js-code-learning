e5b966c55ca31d2668e91a7c01979b9e1b7d8545
MAHESH06-cyber <mchaudhari1201@gmail.com>
2023-09-17 21:34:15 +0530

    conversion of datatypes

---
+++ 02_dataTypes/02_Conversion_Operation.js
@@ -0,0 +1,21 @@
+let score = "40"
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "44abs" => NaN
+//  true => 1 & false => 0
+// null => 0
+// undefiend => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true & 0 =>false
+// "" => false & "dhfsd" => true
