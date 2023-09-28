b3fb11fc1a5be3c903d8bfc0d558e17412fba41e
MAHESH06-cyber <mchaudhari1201@gmail.com>
2023-09-28 10:26:45 +0530

    named iife and this arrow

---
+++ 02_basics/functions.js
@@ -0,0 +1,31 @@
+console.log();
+
+function sayName() {
+    console.log("m");
+    console.log("a");
+    console.log("h");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+// -----------------------------------------
+// sayName();
+
+// function one(a, b) {
+//     return a + b;
+// }
+// console.log(one(12, 12));
+
+// -------------------------------------
+// function two() {
+//     let b = 2
+//     let c = "star"
+//     function three() {
+//         console.log(c);
+//     }
+//     three()
+// }
+// two()
+
+
+
